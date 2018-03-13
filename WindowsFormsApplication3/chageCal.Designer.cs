a6bba4f3653a103cd3661ccf4e17c7a6c422910a
rendarCB <tjddnr0217@gmail.com>
2018-03-13 21:36:25 +0900

    dis_cal

---
+++ WindowsFormsApplication3/chageCal.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class chageCal
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(chageCal));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sum_rt = new System.Windows.Forms.Label();
+            this.elisted = new System.Windows.Forms.DateTimePicker();
+            this.sum = new System.Windows.Forms.Label();
+            this.bta = new System.Windows.Forms.ComboBox();
+            this.percent = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "계산";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "입대일";
+            // 
+            // sum_rt
+            // 
+            this.sum_rt.AutoSize = true;
+            this.sum_rt.Location = new System.Drawing.Point(213, 149);
+            this.sum_rt.Name = "sum_rt";
+            this.sum_rt.Size = new System.Drawing.Size(0, 12);
+            this.sum_rt.TabIndex = 5;
+            // 
+            // elisted
+            // 
+            this.elisted.Location = new System.Drawing.Point(23, 72);
+            this.elisted.Name = "elisted";
+            this.elisted.Size = new System.Drawing.Size(246, 21);
+            this.elisted.TabIndex = 6;
+            // 
+            // sum
+            // 
+            this.sum.AutoSize = true;
+            this.sum.BackColor = System.Drawing.SystemColors.Info;
+            this.sum.Location = new System.Drawing.Point(21, 130);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(0, 12);
+            this.sum.TabIndex = 8;
+            // 
+            // bta
+            // 
+            this.bta.DisplayMember = "육군/의경";
+            this.bta.FormattingEnabled = true;
+            this.bta.Items.AddRange(new object[] {
+            "육군/의경",
+            "해군",
+            "공군",
+            "해병",
+            "사회복무"});
+            this.bta.Location = new System.Drawing.Point(74, 24);
+            this.bta.Name = "bta";
+            this.bta.Size = new System.Drawing.Size(139, 20);
+            this.bta.TabIndex = 9;
+            this.bta.Text = "소속 군대";
+            this.bta.UseWaitCursor = true;
+            
+            // 
+            // percent
+            // 
+            this.percent.Location = new System.Drawing.Point(23, 149);
+            this.percent.Name = "percent";
+            this.percent.Size = new System.Drawing.Size(246, 37);
+            this.percent.TabIndex = 10;
+            // 
+            // chageCal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 209);
+            this.Controls.Add(this.percent);
+            this.Controls.Add(this.bta);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.elisted);
+            this.Controls.Add(this.sum_rt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "chageCal";
+            this.Text = "전역계산기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label sum_rt;
+        private System.Windows.Forms.DateTimePicker elisted;
+        private System.Windows.Forms.Label sum;
+        private System.Windows.Forms.ComboBox bta;
+        private System.Windows.Forms.ProgressBar percent;
+    }
+}
+
---
+++ WindowsFormsApplication3/chageCal.cs
@@ -0,0 +1,239 @@
+﻿// SYSTEM OBJECT MAKE FROM SWFACTORY
+// MADE BY MR.CHO
+// VER 0.5 beta
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class chageCal : Form
+    {
+        public chageCal()
+        {
+            InitializeComponent();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //DateTime elisted;
+            //elisted = DateTime.Parse(textBox1.Text); 필요없음
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+      {
+            //DateTime discharged;
+            //discharged = DateTime.Parse(textBox2.Text); 필요없음 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            percent.Minimum = 0;
+            percent.Maximum = 100;
+            percent.Value = 0;
+            
+            // 1번과정 ---------------기존 연산과정
+            DateTime el = DateTime.Parse(elisted.Text.ToString()); //입대날짜 입력
+            //DateTime di = DateTime.Parse(discharged.Text.ToString()); //전역날짜 입력 수정과정에서 불필요             TimeSpan rt = di - el; // 두날짜 차이 
+            TimeSpan day = DateTime.Now.Subtract(DateTime.Parse(elisted.Text.ToString()));// 현재 날짜 - 입대날짜(군생활한날)
+
+            //2번과정----------------- 입대날짜를 받은뒤 21개월 더해서 연산하는방법 ■코딩과정에서 필요하나 수정/최종 과정에서 불필요 ■
+
+            //DateTime addnow = el.AddMonths(21);
+            //TimeSpan finish = addnow - el;
+            //TimeSpan remainDay = DateTime.Now.Subtract(DateTime.Parse(addnow.ToString()));
+            
+           
+            // 1번과정 ----------------------------------------------기존 연산과정 계산식 ---------------------------
+            //double total = (int)rt.TotalDays; // 두날짜사이의 간격을 double형식으로 변환 
+
+            //double eli = (int)day.TotalDays; // 비슷하게 현재날짜 - 입대날짜 변환
+
+            
+            //double s = eli / total *100 ; // (군생활한날)/두날짜차이(21개월) --> 윤달혹은 달마다 날짜가 달라 차이가 생기므로 빼줘서 계산
+            //double sm = Math.Round(s, 0); // double형식이라 소수점이하도 표현 반올림하여 정수만 표현
+            //if(sm>0) 
+            //{
+            //    sum.Text = "현재 당신은" + sm + "%했습니다.";
+            //}
+            //else if (sm<=0)
+            //{
+            //    sum.Text = "현재 입대하지 않았거나 측정할수 없습니다.";
+            //}
+
+            //2번과정-------------------------- 21개월 더해서 연산한뒤 출력과정 ■코딩과정에서 필요하나 수정/최종 과정에서 불필요 ■
+
+            //double total = (int)finish.TotalDays;
+            //double eli = (int)day.TotalDays;
+            //double remainday = (int)remainDay.TotalDays;
+
+            //double SumCal = eli / total * 100;
+            //double SumCalr = Math.Round(SumCal, 0);
+
+            //if (SumCalr > 0)
+            //{
+            //    sum.Text = "현재 당신은 " + SumCalr + "%했습니다. 그리고 " + -remainday + "일 남았습니다.";
+            //}
+            //else if(SumCalr <=0)
+            //{
+            //    sum.Text = "현재 입대하지 않았거나 전역일 수를 측정할수 없습니다.";
+            //}
+  
+            //최종 과정 (전군 표시후 출력)------------------------------------------------
+
+            string army = bta.SelectedItem as string; // combobox의 형태를 string으로 바꿔줘야됨
+            
+            // 육군/의경/해병 = 21개월  해병 = 23개월  공군/사회복무 = 24개월 
+            
+            if (army == "육군/의경") //육군/의경 계산값
+            {
+                DateTime addnow = el.AddMonths(21);
+                TimeSpan finish = addnow - el;
+                TimeSpan remainDay = DateTime.Now.Subtract(DateTime.Parse(addnow.ToString()));
+                double total = (int)finish.TotalDays;
+                double eli = (int)day.TotalDays;
+                double remainday = (int)remainDay.TotalDays;
+
+                double SumCal = eli / total * 100;
+                double SumCalr = Math.Round(SumCal, 0);
+                int prtI = (int)SumCalr;
+                if (SumCalr > 0 )
+                {
+                    sum.Text = "현재 당신은 " + SumCalr + "%했습니다. 그리고 " + -remainday + "일 남았습니다.";
+                    if(prtI <= 100) // maxium 100을 넘지않기위해 
+                    {
+                    percent.Value += prtI;
+                    percent.CreateGraphics().DrawString(prtI.ToString() + "%", new Font("Arial", (float)10.25, FontStyle.Bold),
+                    Brushes.Black, new PointF(percent.Width / 2 - 10, percent.Height / 2 - 7));
+                    }
+                    else if(prtI >100)
+                    {
+                        sum.Text = "현재 당신은 전역자입니다"; //전역자표시 
+                    }
+                  
+                }
+            }
+            else if (army == "해군") //  해군 계산값
+            {
+                DateTime addnow = el.AddMonths(23);
+                TimeSpan finish = addnow - el;
+                TimeSpan remainDay = DateTime.Now.Subtract(DateTime.Parse(addnow.ToString()));
+                double total = (int)finish.TotalDays;
+                double eli = (int)day.TotalDays;
+                double remainday = (int)remainDay.TotalDays;
+
+                double SumCal = eli / total * 100;
+                double SumCalr = Math.Round(SumCal, 0);
+                int prtI = (int)SumCalr;
+                if (SumCalr > 0)
+                {
+                    sum.Text = "현재 당신은 " + SumCalr + "%했습니다. 그리고 " + -remainday + "일 남았습니다.";
+                    if (prtI <= 100) // maxium 100을 넘지않기위해 
+                    {
+                        percent.Value += prtI;
+                        percent.CreateGraphics().DrawString(prtI.ToString() + "%", new Font("Arial", (float)10.25, FontStyle.Bold),
+                        Brushes.Black, new PointF(percent.Width / 2 - 10, percent.Height / 2 - 7));
+                    }
+                    else if (prtI > 100)
+                    {
+                        sum.Text = "현재 당신은 전역자입니다";
+                    }
+                }
+            }
+            else if (army == "공군") // 공군 계산값
+            {
+                DateTime addnow = el.AddMonths(24);
+                TimeSpan finish = addnow - el;
+                TimeSpan remainDay = DateTime.Now.Subtract(DateTime.Parse(addnow.ToString()));
+                double total = (int)finish.TotalDays;
+                double eli = (int)day.TotalDays;
+                double remainday = (int)remainDay.TotalDays;
+
+                double SumCal = eli / total * 100;
+                double SumCalr = Math.Round(SumCal, 0);
+                int prtI = (int)SumCalr;
+                if (SumCalr > 0)
+                {
+                    sum.Text = "현재 당신은 " + SumCalr + "%했습니다. 그리고 " + -remainday + "일 남았습니다.";
+                    if (prtI <= 100) // maxium 100을 넘지않기위해 
+                    {
+                        percent.Value += prtI;
+                        percent.CreateGraphics().DrawString(prtI.ToString() + "%", new Font("Arial", (float)10.25, FontStyle.Bold),
+                        Brushes.Black, new PointF(percent.Width / 2 - 10, percent.Height / 2 - 7));
+                    }
+                    else if (prtI > 100)
+                    {
+                        sum.Text = "현재 당신은 전역자입니다";
+                    }
+                }
+            }
+            else if (army == "해병") // 해병 계산값
+            {
+                DateTime addnow = el.AddMonths(21);
+                TimeSpan finish = addnow - el;
+                TimeSpan remainDay = DateTime.Now.Subtract(DateTime.Parse(addnow.ToString()));
+                double total = (int)finish.TotalDays;
+                double eli = (int)day.TotalDays;
+                double remainday = (int)remainDay.TotalDays;
+
+                double SumCal = eli / total * 100;
+                double SumCalr = Math.Round(SumCal, 0);
+                int prtI = (int)SumCalr;
+                if (SumCalr > 0)
+                {
+                    sum.Text = "현재 당신은 " + SumCalr + "%했습니다. 그리고 " + -remainday + "일 남았습니다.";
+                    if (prtI <= 100) // maxium 100을 넘지않기위해 
+                    {
+                        percent.Value += prtI;
+                        percent.CreateGraphics().DrawString(prtI.ToString() + "%", new Font("Arial", (float)10.25, FontStyle.Bold),
+                        Brushes.Black, new PointF(percent.Width / 2 - 10, percent.Height / 2 - 7));
+                    }
+                    else if (prtI > 100)
+                    {
+                        sum.Text = "현재 당신은 전역자입니다";
+                    }
+                }
+            }
+            else if(army == "사회복무") // 사회복무 계산값
+            {
+                DateTime addnow = el.AddMonths(24);
+                TimeSpan finish = addnow - el;
+                TimeSpan remainDay = DateTime.Now.Subtract(DateTime.Parse(addnow.ToString()));
+                double total = (int)finish.TotalDays;
+                double eli = (int)day.TotalDays;
+                double remainday = (int)remainDay.TotalDays;
+
+                double SumCal = eli / total * 100;
+                double SumCalr = Math.Round(SumCal, 0);
+                int prtI = (int)SumCalr;
+                if (SumCalr > 0)
+                {
+                    sum.Text = "현재 당신은 " + SumCalr + "%했습니다. 그리고 " + -remainday + "일 남았습니다.";
+                    if (prtI <= 100) // maxium 100을 넘지않기위해 
+                    {
+                        percent.Value += prtI;
+                        percent.CreateGraphics().DrawString(prtI.ToString() + "%", new Font("Arial", (float)10.25, FontStyle.Bold),
+                        Brushes.Black, new PointF(percent.Width / 2 - 10, percent.Height / 2 - 7));
+                    }
+                    else if (prtI > 100)
+                    {
+                        sum.Text = "현재 당신은 전역자입니다";
+                    }
+                }
+            }
+
+            else
+                sum.Text = "현재 입대하지 않았거나 전역일 수를 측정할수 없습니다.";
+           
+        }
+
+      
+    }
+}
